f62a1929b29ab17acc1f0600fd7f65169243df41
Allan McDonald <mcdonald@intheair.ca>
2023-09-12 20:20:33 -0700

    Delete JavaScriptReview directory
    Adding file without answers already contained.

--- JavaScriptReview/js/ex04.js
+++
@@ -1,9 +0,0 @@
-let fruits = ["Banana", "Orange", "Apple", "Mango", "Star Fruit", "Pear", "Peach"];
-
-// for (let i = 0; i < fruits.length; i++) {
-//     console.log(fruits[i]);
-// }
-
-fruits.forEach(el => console.log(el));
-
-fruits.forEach((el, index) => console.log(`${index}. ${el}`));
