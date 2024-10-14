0078971b471620f74c3444b944554fc42979d99f
Allan McDonald <allan.mcdonald@itas.ca>
2024-10-14 15:31:35 -0700

    Updating with Node Web Servers examples

---
+++ 06-NodeWebServers/example02URLModule.js
@@ -0,0 +1,1 @@
+const adr = 'http://localhost:1357/this/is/the/path/myfile.html?year=1919&month=10#nowhere';
---
+++ 06-NodeWebServers/example04WebServer.js
@@ -0,0 +1,14 @@
+
+// TODO: 0. Create web server
+
+// TODO: 1. Get file from a path
+
+// TODO: 1b. Handle file errors
+
+// TODO: 1c. We will need to refactor (well at least we should)
+
+// TODO: 2. handling default files if no file specified
+
+// TODO: 3. Handling extensions/other types of files
+
+// TODO: 4. Logging actions
